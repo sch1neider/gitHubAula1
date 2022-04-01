4a2329025f1117c606d742585c860667ac8d01f4
Gabriel Augusto <gaugustorocha54@gmail.com>
2022-03-31 23:15:13 -0300

    GITHUB Aula1

---
+++ AulasCSharp/Aula1/Program.vb
@@ -0,0 +1,8 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Console.WriteLine("Estou chegando para ganhar 15,000.00")
+    End Sub
+End Module
